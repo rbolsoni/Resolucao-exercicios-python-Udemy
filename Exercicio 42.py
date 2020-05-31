41fcb3c6a74ebdfb88258fc6cc60bde7e811c3df
Rafael Bolsoni <rafabol91@gmail.com>
2020-05-31 09:37:47 -0300

    Delete Exercicio 42.py

--- Exercicio 42.py
+++
@@ -1,9 +0,0 @@
-# Exercicio 42 - Salario base
-
-sal_base = float(input('Digite o valor do salario base:'))
-sal_grat = (sal_base/100)*5
-sal_impos = (sal_base/100)*7
-sal_real = sal_base+sal_grat-sal_impos
-print('O salario final será de: R$', sal_real)
-
-

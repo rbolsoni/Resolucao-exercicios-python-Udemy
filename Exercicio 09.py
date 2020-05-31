2978a6d4652bcadf14b20156097f6d4795182c85
Rafael Bolsoni <rafabol91@gmail.com>
2020-05-31 09:33:48 -0300

    Delete Exercicio 09.py

--- Exercicio 09.py
+++
@@ -1,7 +0,0 @@
-# Exercicio 9 - Transformando graus Celcius em Kelvin
-
-print('Digite a temperatura em graus Celcius:')
-c = int(input())
-k = c+273.25
-print('A temperatura convertida em graus Kelvin é:', k)
-

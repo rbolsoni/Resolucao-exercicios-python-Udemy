e6e4db8d0173883eed3696188eed5f76ccb7add2
Rafael Bolsoni <rafabol91@gmail.com>
2020-05-31 09:34:18 -0300

    Delete Exercicio 12.py

--- Exercicio 12.py
+++
@@ -1,7 +0,0 @@
-# Exercicio 12 - Convertendo Milhas em
-
-print('Digite uma distancia em milhas para converte-la em km:')
-m = int(input())
-km = 1.61*m
-print('A distancia convetida em km é:', km)
-

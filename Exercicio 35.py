c1b6d03cc928dd610c536891da78e4143761c334
Rafael Bolsoni <rafabol91@gmail.com>
2020-05-31 09:36:44 -0300

    Delete Exercicio 35.py

--- Exercicio 35.py
+++
@@ -1,7 +0,0 @@
-# Exercicio 35 - Calculo da hipotenusa
-
-a = float(input('Para saber o valor da hipotenusa digite o valor de um lado do triangulo:'))
-b = float(input('Agora digite o valor do outro lado:'))
-h = (((a ** 2) + (b ** 2)) ** 0.5)
-print('O valor da hipotenusa é:', h)
-

a06185c0719e877417773acfc39f2c6b758d03e5
Rafael Bolsoni <rafabol91@gmail.com>
2020-05-31 09:34:26 -0300

    Delete Exercicio 17.py

--- Exercicio 17.py
+++
@@ -1,7 +0,0 @@
-# Exercicio 17 - centimetros em polegadas
-
-print('Digite o comprimento em centimetros para converte-lo em polegadas:')
-cm = int(input())
-p = cm/2.54
-print('O comprimeiro em polegadas é:', p)
-

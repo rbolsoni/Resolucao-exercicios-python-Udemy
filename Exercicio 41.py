f3c604e7c78e24639d89a34c0e563e1544634132
Rafael Bolsoni <rafabol91@gmail.com>
2020-05-31 09:37:45 -0300

    Delete Exercicio 41.py

--- Exercicio 41.py
+++
@@ -1,7 +0,0 @@
-# Exercicio 41 - Horas trabalhadas
-
-valor_h = (float(input('Digite o valor da hora trabalhada:')))
-num_ht = (float(input('Digite o numero de horas trabalhadas:')))
-vp = (valor_h * num_ht) + (((valor_h * num_ht) / 100) * 10)
-print('O valor pago para o funcionario é: R$', vp)
-

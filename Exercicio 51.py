bf20ef2a6332a13e6191508965d4d25175b38af7
Rafael Bolsoni <rafabol91@gmail.com>
2020-05-31 09:38:02 -0300

    Delete Exercicio 51.py

--- Exercicio 51.py
+++
@@ -1,9 +0,0 @@
-# Exercicio 51 - Coordenadas
-
-x = float(input('Digite a coordenada X: '))
-y = float(input('Digite a coordenada Y: '))
-
-hipo = (x ** 2) + (y ** 2)
-hipo = hipo ** 0.5
-
-print(f'A distancia da origem é de: {hipo}')

a0145e90491d2db28640ed9f15b968367b57f0a4
Rafael Bolsoni <rafabol91@gmail.com>
2020-05-31 09:34:23 -0300

    Delete Exercicio 15.py

--- Exercicio 15.py
+++
@@ -1,7 +0,0 @@
-# Exercicio 15 - Convertendo radianos em graus
-
-print('Digite o angulo em radianos para converte-lo em graus:')
-r = int(input())
-g = r*180/3.14
-print('O angulo em graus é:', g)
-

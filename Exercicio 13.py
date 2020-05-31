c015ca9eba94b6501aa650dd112ed19f1e0bcd6b
Rafael Bolsoni <rafabol91@gmail.com>
2020-05-31 09:34:20 -0300

    Delete Exercicio 13.py

--- Exercicio 13.py
+++
@@ -1,7 +0,0 @@
-# Exercicio 13 - Convertendo km e milhas
-
-print('Digite a distancia em km para converte-la em milhas:')
-km = int(input())
-m = km/1.61
-print('A distancia convertida em Milhas é:', m)
-

bfd4fc8045b7ed714556a8cf8e1babb6bc6ceada
Rafael Bolsoni <rafabol91@gmail.com>
2020-05-31 09:37:50 -0300

    Delete Exercicio 44.py

--- Exercicio 44.py
+++
@@ -1,7 +0,0 @@
-# Exercicio 44 - Altura dos degraus
-
-ad = float(input('Qual a altura de cada degrau da escada?'))
-ap = float(input('Qual a altura desejada?'))
-qd = int(ap/ad)
-print('Você deverá subir', qd, 'degraus.')
-

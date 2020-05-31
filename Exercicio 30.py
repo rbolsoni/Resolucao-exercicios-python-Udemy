9bab8e8e1d326e3ecefa62cee2b7a1b2be2cda86
Rafael Bolsoni <rafabol91@gmail.com>
2020-05-31 09:35:58 -0300

    Delete Exercicio 30.py

--- Exercicio 30.py
+++
@@ -1,9 +0,0 @@
-# Exercicio 30 - Transformando real em dolar
-
-print('Digite o valor em reais:')
-brl = int(input())
-print('Digite a cotação do dolar:')
-cot = float(input())
-dol = brl*cot
-print('O valor em dolares é: $',dol)
-

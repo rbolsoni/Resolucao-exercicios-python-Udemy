9bb599e06e33e68d82e3f39f683a227c8c13f762
Rafael Bolsoni <rafabol91@gmail.com>
2020-05-31 09:38:00 -0300

    Delete Exercicio 50.py

--- Exercicio 50.py
+++
@@ -1,8 +0,0 @@
-# Exercicio 50 - idade
-
-idade = int(input('Digite a sua idade:'))
-ano_atual = int(input('Digite o ano atual:'))
-ano_nasc = ano_atual - idade
-print(f'O ano do seu nascimento é: {ano_nasc}')
-
-

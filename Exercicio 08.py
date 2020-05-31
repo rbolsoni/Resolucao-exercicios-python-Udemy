0b94c39dae93def7fa3fecc87f92a179d5576011
Rafael Bolsoni <rafabol91@gmail.com>
2020-05-31 09:33:44 -0300

    Delete Exercicio 08.py

--- Exercicio 08.py
+++
@@ -1,6 +0,0 @@
-# Exercicio 8 - Transformando Kelvin em Celcius
-
-print('Digite uma temperatura em graus Kelvin:')
-k = int(input())
-c = k-273.15
-print('A temperatura convertida em graus Celcius é', c)

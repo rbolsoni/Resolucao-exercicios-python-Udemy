df582982fc5ccf17de50e8068bf24ce24347dbb9
Rafael Bolsoni <rafabol91@gmail.com>
2020-05-31 09:36:48 -0300

    Delete Exercicio 37.py

--- Exercicio 37.py
+++
@@ -1,6 +0,0 @@
-# Exercicio 37 - 12% de desconto em um produto
-
-vp = float(input('Para saber o valor do desconto, digite o valor do produto:'))
-vd = (vp/100)*88
-print('O valor do produto com o desconto é de:', vd)
-

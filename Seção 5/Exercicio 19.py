5b4ebc270414f3886fd2eb63cfd700abe2848e04
Rafael Bolsoni <rafabol91@gmail.com>
2020-06-05 18:08:28 -0300

    Add files via upload

--- Seção 5/Exercicio 19.py
+++ Seção 5/Exercicio 19.py
@@ -1,15 +1,13 @@
 # Exercicio 19
 
-import sys
-import math
-
 num = int(input('Digite um valor para saber se é divisivel por 5 ou por 3:'))
 div3 = num % 3
 if div3 == 0:
     print('Esse numero é divisivel por 3.')
-    sys.exit()
 
 div5 = num % 5
 if div5 == 0:
     print('Esse numero é divisivel por 5.')
-    sys.exit()
+
+else:
+    print('Esse numero não é divisivel por 3 e nem por 5')

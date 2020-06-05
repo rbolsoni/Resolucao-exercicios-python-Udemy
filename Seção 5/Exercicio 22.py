2dbd2ca62dcdc325eb6b7e0c6fba32d45f60d779
Rafael Bolsoni <rafabol91@gmail.com>
2020-06-05 19:02:47 -0300

    Add files via upload

--- Seção 5/Exercicio 22.py
+++ Seção 5/Exercicio 22.py
@@ -1,2 +1,18 @@
 # Exercicio 22 - aposentadoria
 
+idade = float(input('Digite a idade do trabalhador: '))
+tempo = float(input('Digite o tempo de serviço: '))
+
+if idade > 65:
+    print('Pode se aposentar!')
+
+elif tempo > 30:
+    print('Pode se aposentar!')
+
+elif idade > 60 and tempo > 25:
+    print('Pode se aposentar!')
+
+else:
+    print('Não pode se aposentar!')
+
+
